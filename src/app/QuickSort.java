e687a1587589fb1c2e193920316c6ef87b65c638
Felipe Moura <felipecandidodemoura@gmail.com>
2025-01-13 17:15:51 -0300

    Substituir metodo por objeto metodo; extrair metodo logo em seguida

--- src/app/QuickSort.java
+++ src/app/QuickSort.java
@@ -16,31 +16,45 @@ public class QuickSort {
 		return valores.length;
 	}
 
-	//O particionamento do quicksort consiste em alocar o pivo em uma posicao 
-	//na qual a parte anterior é composta de valores menores do que ele e a 
-	//parte posterior, de valores maiores do que ele. 
-	public int particionamento(int left, int right) {
+	public int[] ordenar(int left, int right) {
+		
+		if (left < right) {
+			Particionamento parti = new Particionamento(left, right, valores);
+			
+			int indicePivot = parti.execute();
+			ordenar(left, indicePivot -1);
+			ordenar(indicePivot + 1, right);
+		}
+		return valores;
+	}
+}
 
-		//1º passo: determinar o pivo e inicializar a variavel que controla a troca
-		// de elementos. 
-		
-		//Essas duas instruções abaixo nao permitem que sejam aplicado 
-		//"Extrair Método" nelas, de modo a dividir o método "particionamento"
-		//em passos menores. 
-		int pivot = valores[left];
-		int i = left;
-		
-		//2º passo: varrer o pivo e realizar as trocas de elementos sempre que necessário
+class Particionamento {
+	
+	private int left, right;
+	private int[] valores;
+	
+	private int pivot;
+	private int i;
+
+	public Particionamento(int left, int right, int[] valores){
+		this.left = left;
+		this.right = right;
+		this.valores = valores;
+	}
+
+	private void init(){
+		this.pivot = valores[left];
+		this.i = left;
+	}
+
+	private void scan(){
 		for (int j=i+1; j<=right; j++) {
 			if (valores[j] <= pivot) {
 				i++; 
 				swap(i, j); 
 			}
 		}
-		
-		//3º passo: trocar o pivo com o elemento da ultima posicao dos numeros menores que ele
-		swap(left, i);
-		return i;
 	}
 
 	private void swap(int i, int j) {
@@ -49,13 +63,14 @@ public class QuickSort {
 		valores[j] = temp;
 	}
 
-	public int[] ordenar(int left, int right) {
-		if (left < right) {
-			int indicePivot = particionamento(left, right);
-			ordenar(left, indicePivot -1);
-			ordenar(indicePivot + 1, right);
-		}
-		return valores;
-	}
+	public int execute(){
+		this.init();
 
-}
+		this.scan();
+
+		swap(left, i);
+		return i;
+	}
+}	
+
+
